d622a5c8caade61a668ff92a50f52e31d133f3a9
jeannehostettler <122967561+jeannehostettler@users.noreply.github.com>
2024-06-12 12:43:37 +0200

    création du carte pour le shop

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,19 @@ var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
 };
 burger.addEventListener("click", toggleMenu);
+$(".carousel").carousel({
+  interval: 2000,
+});
+
+import Swiper, { Navigation, Pagination } from "swiper";
+
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
