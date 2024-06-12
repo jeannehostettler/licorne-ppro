10e912098b6db64e72d1deda9e45951bd1cc63d3
jeannehostettler <122967561+jeannehostettler@users.noreply.github.com>
2024-06-12 14:42:55 +0200

    création des deux premières cartes pour le shop

--- src/js/main.js
+++ src/js/main.js
@@ -7,16 +7,3 @@ burger.addEventListener("click", toggleMenu);
 $(".carousel").carousel({
   interval: 2000,
 });
-
-import Swiper, { Navigation, Pagination } from "swiper";
-
-Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper(".swiper-container", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
